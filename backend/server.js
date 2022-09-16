625d701e0290627fff92e4ad8ac4f53e0640aa7f
Alex Huynh <abhuynh@iastate.edu>
2022-09-15 22:06:00 -0500

    Create files directory if it doesn't exist

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const cors = require("cors")
+const fs = require("fs");
 const port = 2468
 
 const app = express();
@@ -13,7 +14,9 @@ app.post("/upload", (req, res) => {
     if (!req.files) {
       return res.status(400).send("No files were uploaded.");
     }
-
+    if (!fs.existsSync(__dirname + "/../files/")) {
+      fs.mkdirSync(__dirname + "/../files/");
+    }
     const file = req.files.file;
     const path = __dirname + "/../files/" + file.name;
 
