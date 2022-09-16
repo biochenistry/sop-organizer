1873844f6cac8a0fce13aedb2ccfb73f75545f1f
Alex Huynh <abhuynh@iastate.edu>
2022-09-15 21:01:04 -0500

    File uploads to parent directory

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const cors = require("cors")
-const path = require("path");
 const port = 2468
 
 const app = express();
@@ -11,20 +10,19 @@ app.use(
 );
 
 app.post("/upload", (req, res) => {
-  console.log(req);
     if (!req.files) {
       return res.status(400).send("No files were uploaded.");
     }
 
     const file = req.files.file;
-    const path = __dirname + "/files/" + file.name;
+    const path = __dirname + "/../files/" + file.name;
 
     file.mv(path, (err) => {
       if (err) {
         return res.status(500).send(err);
       }
       return res.send({ status: "success", path: path });
-  });
+    });
 });
 
 app.listen(port, () => {
