005955cd71bc0f2a818a18de4cf43dc5c6977364
Christian Lisle <calisle@iastate.edu>
2022-10-20 14:00:59 +0000

    Show name of user that marked file for deletiom

---
+++ frontend/src/services/users.ts
@@ -0,0 +1,12 @@
+import { User } from '../types/index';
+
+const BASE_URL = process.env.NUXT_ENV_API_URL;
+
+export async function getUser(id): User {
+  let res = await fetch(`${BASE_URL}/users/${id}`);
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
+  const user: User = await res.json();
+  return user;
+};
