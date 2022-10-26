3385e4a6f910e5e5d429609dbc880e318cceafdc
Alex <abhuynh@iastate.edu>
2022-10-25 21:35:25 -0500

    Hopeful fix for CI/CD

--- frontend/src/services/users.ts
+++ frontend/src/services/users.ts
@@ -2,7 +2,7 @@ import { User } from '../types/index';
 
 const BASE_URL = process.env.NUXT_ENV_API_URL;
 
-export async function getUser(id): User {
+export async function getUser(id): Promise<User> {
   let res = await fetch(`${BASE_URL}/users/${id}`);
   if (!res.ok) {
     throw new Error(res.statusText);
