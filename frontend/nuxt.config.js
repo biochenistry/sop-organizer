666c124abb6b823766b373ddc827061fff640ebc
Christian Lisle <calisle@iastate.edu>
2022-11-01 17:49:26 +0000

    Font improvements

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -38,6 +38,7 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
+    treeShake: true,
     customVariables: ['~/assets/variables.scss'],
     theme: {
       themes: {
